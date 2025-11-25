dcd193aaa1f0431d82c52fb9bb6c832be2598b74
Gourav Nutulapati <gourav.nutulapati@gmail.com>
2025-11-25 19:52:06 +0530

    Revert App.java and rebuild JAR for CheerpJ debugging

--- App.java
+++ App.java
@@ -8,21 +8,17 @@ public class App {
         int boardWidth = columnCount * tileSize;
         int boardHeight = rowCount * tileSize;
 
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                JFrame frame = new JFrame("Pac Man");
-                // frame.setVisible(true);
-                frame.setSize(boardWidth, boardHeight);
-                // frame.setLocationRelativeTo(null); // Causes NPE in CheerpJ
-                frame.setResizable(false);
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JFrame frame = new JFrame("Pac Man");
+        // frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-                PacMan pacmanGame = new PacMan();
-                frame.add(pacmanGame);
-                frame.pack();
-                pacmanGame.requestFocus();
-                frame.setVisible(true);
-            }
-        });
+        PacMan pacmanGame = new PacMan();
+        frame.add(pacmanGame);
+        frame.pack();
+        pacmanGame.requestFocus();
+        frame.setVisible(true);
     }
 }
